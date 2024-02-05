45e08d0026b6941ee6df8eefb3a00c2c7ee52335
bruno <brunogon542@gmail.com>
2024-02-05 08:52:13 -0300

    Revisao

---
+++ Revisaoo/src/revisaoo/Revisaoo.java
@@ -0,0 +1,248 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package revisao_2024_java_2;
+
+
+
+/**
+ *
+ * @author 962576
+ */
+public class Revisao_2024_java_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+/*EX1
+        System.out.println("Hello World!!");
+         */
+
+        /*EX2
+        //System.out.println("Hello World!!");
+        /*System.out.println("Hello World!!");*/
+        /*EX3
+        String carName = "Volvo";
+         */
+        
+        /*EX4
+        int maxSpeed = 120;
+         */
+        
+        /*EX5
+        int x = 5;
+        int y = 10;
+        System.out.println(x + y);
+         */
+        
+        /*EX6
+        int x = 5;
+        int y = 10;
+        int z = x + y;
+        System.out.println(z);
+         */
+        
+        /*EX7
+        int x = 5int y = 6,int z = 50;
+        System.out.println(x + y + z);.
+         */
+        
+        /*EX8
+        int myNum = 9;
+        float myFloatNum = 8.99f;
+        char myLetter = 'A';
+        boolean myBool = false;
+        String myText = "Hello World!!";
+         */
+        
+        /*EX9
+        primitivos datas types.
+         */
+        
+        /*EX10
+        double myDouble = 9.78d;
+        int myInt = (int) myDouble;
+         */
+        
+        /*EX11
+        System.out.println(10 * 5);
+         */
+        
+        /*EX12
+        System.out.println(10 / 5);
+         */
+        
+        /*EX13
+        int x = 10;
+        System.out.println("x: " +x);
+         */
+        
+        /*EX14
+        int x;
+        x = 10;
+        System.out.println("x: "+x+"5");
+         */
+        
+        /*EX15
+        String greeting = "Hello";
+         */
+        
+        /*EX16
+        String txt = "Hello";
+        System.out.println(txt.toUpperCase());
+         */
+        
+        /*EX17
+        String firstName = "John";
+        String lastName = "Doe";
+        System.out.println(firstName + lastName);
+         */
+        
+        /*EX18
+        String firstName = "John";
+        String lastName = "Doe";
+        System.out.println(firstName+(lastName));
+         */
+        
+        /*EX19
+        String txt = "Hello Everybody";
+        System.out.println(txt.indexOf("e"));
+         */
+        
+        /*EX20
+        int x = 5;
+        int y = 10;
+        Math.addExact(x, y);
+         */
+        
+        /*EX21
+        int x = 16;
+        System.out.println(Math.sqrt(x));
+         */
+        
+        /*EX22
+        System.out.println(Math.random());
+         */
+        
+        /*EX23
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println(isJavaFun);
+        System.out.println(isFishTasty);
+         */
+        
+        /*EX24
+        int x = 10;
+        int y = 9;
+        System.out.println(x > y);
+         */
+        
+        /*EX25
+        int x = 50;
+        int y = 10;
+        if(x > y){
+        System.out.println("Hello World!!");
+        }
+         */
+        
+        /*EX26
+        int x = 50 ;
+        int y = 50;
+        if(x == y){
+        System.out.println("Hello World!!");
+        }
+         */
+        
+        /*EX27
+        int x = 50 ;
+        int y = 50;
+        if(x == y){
+        System.out.println("Yes");
+        }else{
+        System.out.println("No");
+        }
+         */
+        
+        /*EX28
+        int x = 50 ;
+        int y = 50;
+        if(x == y){
+        System.out.println("1");
+        }if(x > y){
+        System.out.println("2");
+        }else{
+        System.out.println("3");
+        }
+         */
+        
+        /*EX29
+         */
+        
+        /*EX30
+        int day = 2;
+        switch (day) {
+        case 1:
+        System.out.println("Saturday");
+        break;
+        case 2:
+        System.out.println("Sunday");
+        break;
+        }
+         */
+        
+        /*EX31
+        int day = 4;
+        switch (day) {
+        case 1:
+        System.out.println("Saturday");
+        break;
+        case 2:
+        System.out.println("Sunday");
+        break;
+        case 3:
+        System.out.println("");
+        break;
+        case 4:
+        System.out.println("Weekend");
+        break;
+        }
+         */
+        
+        /*EX32
+        int i = 1;
+        while (i < 6) {
+        System.out.println(i);
+        i++;
+        }
+         */
+        
+        /*EX33
+        int i = 1;
+        do {
+        System.out.println(i);
+        i++;
+        }
+        while(i < 6);
+         */
+        
+        /*EX34
+        String i = "Sim";
+        for(int count=5 ; count >= 1; count--){
+        System.out.println(i);
+        }
+         */
+        
+        /*EX35
+        ffgh
+         */
+    
+    
+    
+    }
+       
+    }
+    
+
